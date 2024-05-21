cb4a17af00454e1f2059c4ad6a4ceeb63e57d6c1
charly reux <charlyreux@gmail.com>
2024-05-21 11:59:02 +0200

    devcontainer setup

--- .devcontainer/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [ -n "$CODESPACES" ]; then
-    gh codespace ports visibility 3000:public -c $CODESPACE_NAME > /dev/null 2>&1
-    
-fi
--- frontend/config.js
+++ frontend/config.js
@@ -1,1 +1,10 @@
-export const BACKEND_URL = "http://localhost:3000";//Change depending on your environment
+
+
+  if (process.env.CODESPACE_NAME) {
+    const BACKEND_URL = `http://${process.env.CODESPACE_NAME}-3000.app.github.dev`;
+
+  } else {
+
+    const BACKEND_URL = "http://localhost:3000";
+  }
+export default BACKEND_URL
